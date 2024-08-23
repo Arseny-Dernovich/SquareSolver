2dcc334ac3febc4984dffa921fbedfed5ca25b02
Arseny-Dernovich <arseni.dernovich@bk.ru>
2024-08-23 21:48:21 +0300

    неполная квадратка в линкере

---
+++ Solve_Square.cpp
@@ -0,0 +1,207 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+#include <ctype.h>
+
+#include "header.h"
+
+
+enum type_equation {
+ incomp_equat = 6 ,
+ full_equat
+};
+
+
+
+
+
+const double EPS = 1e-12;
+
+void input_coeff(double* a , double* b , double* c)
+{
+    /// assert (c != NULL / nullptr / 0 / c);
+    /// assert!!!!!
+    /// assert!!!!!
+    char ch = 0;
+    printf ("Эта программа решает квадратное уравнение\n");
+    while (true)
+    {
+        printf ("Введите значения коэффицентов \nквадратного уравнения ax^2 + bx + c (a,b,c):");
+        if ((scanf ("%lf %lf %lf", a, b, c)) != 3) {
+            printf ("Неверные входные значения коэффицентов\n(коэффиценты могут являться только числами)\n");
+            while (ch = getchar() != '\n');
+        }
+
+        else {
+            while (ch = getchar() != '\n');
+        }
+
+            return ;
+    }
+}
+
+
+_Bool is_zero (double coeff)
+{
+    if (fabs(coeff) <=  EPS)
+        return true;
+
+    else
+        return false;
+}
+
+
+int check_discriminant (coeffs coef)
+{
+    double discriminant = (coef.b * coef.b) - (4 * coef.a * coef.c);
+
+    if (is_zero (discriminant))
+        return bool_zero_discr;
+
+    else if (discriminant > 0)
+        return bool_pos_discr;
+
+    else if (discriminant < 0)
+        return bool_neg_discr;
+
+    return 0;
+}
+
+
+int choose_type_equation (coeffs coef)
+{
+    if (is_zero (coef.a)) {
+        if (!is_zero (coef.b) && !is_zero (coef.c))
+            return line_roots;
+
+        else if (is_zero (coef.b) && is_zero (coef.c))
+            return infinit_roots;
+
+        else if (is_zero (coef.b) && !is_zero (coef.c))
+            return no_roots;
+
+        return 0;
+    }
+
+    else
+        return check_discriminant (coef);
+
+}
+
+
+int calculate_roots (coeffs coef , double* x1 , double* x2)
+{
+    double discriminant = (coef.b * coef.b) - (4 * coef.a * coef.c);
+
+    int check = choose_type_equation (coef);
+
+    *x1 = check;
+    *x2 = check;
+
+    switch (check) {
+
+        case line_roots:
+            *x1 = (-coef.c) / coef.b;
+
+            break;
+
+        case bool_pos_discr:
+        {
+            double sqrt_discriminant = sqrt (discriminant);
+            *x1 = (-coef.b - sqrt_discriminant) / (2 * coef.a);
+            *x2 = (-coef.b + sqrt_discriminant) / (2 * coef.a);
+
+            break;
+        }
+
+        case bool_zero_discr:
+            *x1 = (-coef.b) / (2 * coef.a);
+
+            break;
+
+        case infinit_roots:
+        case no_roots:
+        case bool_neg_discr:
+
+            /**x1 == NAN;
+            *x2 == NAN;
+
+            break;*/
+
+        default :
+
+            break;
+    }
+
+    return check;
+}
+
+
+
+void output (coeffs coef , roots* root , int check)
+{
+
+
+    switch (check) {
+
+    case line_roots:
+        printf ("Корень линейного уравнения %.1lfx + %.1lf=0   x = %.2lf\n", coef.b, coef.c, root->x1);
+        break;
+    case infinit_roots:
+        printf ("Уравнение имеет бесконечно много решений (все коэффициенты равны нулю).\n");
+        break;
+    case no_roots:
+        printf ("Уравнение не имеет решений (c != 0, а старшие коэффициенты равны нулю).\n");
+        break;
+    case incomp_equat:
+        printf ("Корень квадратного уравнения %.1lfx^2 + %.1lfx + %.1lf = 0 111   x = %.2lf\n", coef.a, coef.b, coef.c, root->x2);
+        break;
+    case bool_pos_discr:
+        printf ("Корни квадратного уравнения %.1lfx^2 + %.1lfx + %.1lf = 0  -  x1 = %.2lf и x2 = %.2lf\n", coef.a, coef.b, coef.c, root->x1, root->x2);
+        break;
+    case bool_neg_discr:
+        printf ("Корни квадратного уравнения %.1lfx^2 + %.1lfx + %.1lf = 0  -  x1 = %.2lf и x2 = %.2lf\n", coef.a, coef.b, coef.c, root->x1, root->x2);
+        printf ("Корней нет (дискриминант отрицательный).\n");
+        break;
+    case bool_zero_discr:
+        printf ("Корень квадратного уравнения %.1lfx^2 + %.1lfx + %.1lf = 0  -  x1 = %.2lf \n", coef.a, coef.b, coef.c, root->x1);
+        break;
+    }
+}
+
+
+_Bool continue_entering (void)
+{
+    while (true)
+    {
+        char choose;
+        printf ("Если хотите продолжить нажмите y/Y , если хотите закончить нажмите n/N\n");
+        scanf (" %c" , &choose);
+        while (getchar() != '\n')
+                continue;
+
+        if (choose == 'y' || choose == 'Y') {
+            return true;
+        }
+        else if ( choose == 'n' || choose == 'N'){
+            return false;
+        }
+
+    }
+}
+
+
+void SolveSquare (coeffs coef , roots* root)
+{
+
+    bool flag = true;
+    while (flag) {
+        input_coeff (&coef.a , &coef.b , &coef.c);
+        /*тип_уравнения =*/ int check = calculate_roots (coef , &root->x1 , &root->x2);
+        /*output (coeffs.a, coeffs.b, coeffs.c /*, тип_уравнения*/
+        output (coef , root , check);
+        flag = continue_entering ();
+    }
+    printf ("Программа завершенна\n");
+
+}
--- Unit_Tests.cpp
+++ Unit_Tests.cpp
@@ -2,18 +2,184 @@
 #include <stdbool.h>
 #include <math.h>
 #include <ctype.h>
-#include "test_values.h"
-#include "funcs_square_eq.h"
+
+#include "header.h"
+
+
+enum type_equation {
+ incomp_equat = 6 ,
+ full_equat
+};
+
+enum checking_roots {
+ bool_line = 8,
+ bool_infinit ,
+ bool_same ,
+ bool_two ,
+ bool_no
+};
+
+enum check_Error {
+ No_Error ,
+ Error
+};
+
+
+const int nTests = 20;
+
+
+const double EPS = 1e-12;
+
+
+int choose_type_equation (test_values data);
+int calculate_roots (test_values data , double* x1 , double* x2);
 
 
 
 
-
-int main (void)
+_Bool compare_roots (double x , double x_verief)
 {
-    Unit_tests (tests);
+    if (fabs (x - x_verief) <= EPS)
+        return true;
 
-    return 0;
+    else
+        return false;
+}
+
+
+void output_Error (test_values data)
+{
+    printf ("\nError test %d :\nкорни уравнения %.1lfx^2 + %.1lfx + %.1lf  не совпадают\n",data.num_test , data.a , data.b , data.c);
+}
+
+
+void output_line_zeroDis (test_values data , double x1)
+{
+    printf ("Эталонный корень : x_refer = %.2lf" , data.x1_refer);
+    printf ("        Вычисленный корень : x = %.2lf\n" , x1);
+}
+
+
+void output_posDis (test_values data , double x1 , double x2)
+{
+    printf ("Эталонные корни : data.x1_refer = %.2lf и data.x2_refer = %.2lf" , data.x1_refer , data.x2_refer);
+    printf ("        Вычисленные корни : x1 = %.2lf и x2 = %.2lf\n" , x1 , x2);
+}
+
+
+void output_posDis_zeroDis_line (test_values data)
+{
+    printf ("\nTest %d :\nОшибок нет , корни уравнения %.1lfx^2 + %.1lfx + %.1lf совпадают\n", data.num_test , data.a , data.b , data.c);
+}
+
+
+
+int Find_Error_Zero_discr (test_values data , double x1 , double x2)
+{
+    if (compare_roots (x1 , data.x1_refer) && compare_roots (x2 , data.x2_refer)) {
+        output_posDis_zeroDis_line (data);
+        output_line_zeroDis (data , x1);
+
+        return No_Error;
+    }
+
+    else {
+        output_Error (data);
+        output_line_zeroDis (data , x1);
+
+        return Error;
+    }
+
+
+}
+
+int Find_Error_Pos_discr (test_values data , double x1 , double x2)
+{
+    if (compare_roots (x1 , data.x1_refer) && compare_roots (x2 , data.x2_refer)) {
+        output_posDis_zeroDis_line (data);
+        output_posDis (data , x1 , x2);
+
+        return No_Error;
+    }
+
+    else {
+        output_Error (data);
+        output_posDis (data , x1 , x2);
+
+        return Error;
+    }
+
+
+}
+
+
+int Find_Error_Neg_discr (test_values data , double x1 , double x2)
+{
+    if (compare_roots (x1 , data.x1_refer) && compare_roots (x2 , data.x2_refer)) {
+        printf ("\nTest %d :\nОшибок нет , корни уравнения %.1lfx^2 + %.1lfx + %.1lf совпадают и являются мнимыми\n", data.num_test , data.a , data.b , data.c);
+
+        return No_Error;
+    }
+
+    else {
+        output_Error (data);
+
+        return Error;
+    }
+
+
+}
+
+
+int Find_Error_Line_roots (test_values data , double x1 , double x2)
+{
+    if (compare_roots (x1 , data.x1_refer) && compare_roots (x2 , data.x2_refer)) {
+        output_posDis_zeroDis_line (data);
+        output_line_zeroDis (data , x1);
+
+        return No_Error;
+    }
+
+    else {
+        output_Error (data);
+        output_line_zeroDis (data , x1);
+
+        return Error;
+    }
+}
+
+
+int Find_Error_Infinit_roots (test_values data , double x1 , double x2)
+{
+    if (compare_roots (x1 , data.x1_refer) && compare_roots (x2 , data.x2_refer)) {
+        printf ("\nTest %d :\nОшибок нет , в уравнении %.1lfx^2 + %.1lfx + %.1lf\nв обоих случаях бесконечное кол-во корней\n", data.num_test , data.a , data.b , data.c);
+
+        return No_Error;
+    }
+
+    else {
+        output_Error (data);
+
+        return Error;
+    }
+
+}
+
+
+int Find_Error_No_roots (test_values data , double x1 , double x2)
+{
+    if (compare_roots (x1 , data.x1_refer) && compare_roots (x2 , data.x2_refer)) {
+        printf ("\nTest %d :\nОшибок нет , в уравнении %.1lfx^2 + %.1lfx + %.1lf\nв обоих случаях нет корней\n", data.num_test , data.a , data.b , data.c);
+
+        return No_Error;
+    }
+
+    else {
+        output_Error (data);
+
+        return Error;
+    }
+
 }
 
 
@@ -21,5 +187,69 @@ int main (void)
 
 
 
+int unit_test (test_values data)
+{
+    int choose = 0;
+    double x1 = 0 , x2 = 0;
+
+    calculate_roots (data , &x1 , &x2);
+    choose = choose_type_equation (data);
+
+    switch (choose) {
+
+        case line_roots:
+
+            return Find_Error_Line_roots (data , x1 , x2);
+
+        case bool_pos_discr:
+
+            return Find_Error_Pos_discr (data , x1 , x2);
+
+        case bool_zero_discr:
+
+            return Find_Error_Zero_discr (data , x1 , x2);
+
+        case infinit_roots:
+
+            return Find_Error_Infinit_roots (data , x1 , x2);
+
+        case no_roots:
+            return Find_Error_No_roots (data , x1 , x2);
+
+        case bool_neg_discr:
+
+            return Find_Error_Neg_discr (data , x1 , x2);
+
+        default :
+
+            return 0;
+    }
 
 
+}
+
+
+int count_number_Errors (const test_values* data)
+{
+    int amount_Errors = 0;
+    for (int i = 0 ; i < nTests ; i++)
+        amount_Errors += unit_test (data[i]);
+
+    return amount_Errors;
+}
+
+
+void output_number_Errors (const test_values* data)
+{
+    printf ("\nКоличество ошибок - %d\n" , count_number_Errors (data));
+}
+
+
+void Unit_tests (const test_values* data)
+{
+    for (int i = 0 ; i < nTests ; i++)
+        unit_test (data[i]);
+
+
+    output_number_Errors (data);
+}
